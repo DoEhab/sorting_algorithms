6448e7f65b84a3dc9540344b7177a1de8cf72b2b
doehab <dohaehab@gmail.com>
2024-10-26 07:25:33 -0700

    bubble sort task

---
+++ 0-bubble_sort.c
@@ -0,0 +1,23 @@
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+
+	i = 0;
+	while (i < size)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j + 1] < array[j])
+			{
+				temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+		i = i + 1;
+	}
+}
