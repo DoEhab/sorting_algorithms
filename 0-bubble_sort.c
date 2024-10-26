b45a4eddcda7985ebe2cd69c8a9f41239cb331ff
doehab <dohaehab@gmail.com>
2024-10-26 08:04:47 -0700

    sorting task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,7 +8,7 @@ void bubble_sort(int *array, size_t size)
 	i = 0;
 	while (i < size)
 	{
-		for (j = 0; j < size - 1; j++)
+		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j + 1] < array[j])
 			{
