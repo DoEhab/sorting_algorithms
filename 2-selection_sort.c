d47f58fdd9da09dfd1db6951efac4e9185c95a7d
doehab <dohaehab@gmail.com>
2024-10-27 13:23:33 -0700

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,25 +10,25 @@
 
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_index;
-	int tmp;
+        size_t i, j, min_index;
+        int tmp;
 
-	for (i = 0; i < size - 1; i++)
-	{
-		min_index = i;
-		for (j = i + 1; j < size; j++)
-		{
-			if (array[min_index] > array[j])
-			{
-				min_index = j;
-			}
-		}
-		if (min_index != i)
-		{
-			tmp = array[i];
-			array[i] = array[min_index];
-			array[min_index] = tmp;
-			print_array(array, size);
-		}
-	}
+        for (i = 0; i < size - 1; i++)
+        {
+                min_index = i;
+                for (j = i + 1; j < size; j++)
+                {
+                        if (array[min_index] > array[j])
+                        {
+                                min_index = j;
+                        }
+                }
+                if (min_index != i)
+                {
+                        tmp = array[i];
+                        array[i] = array[min_index];
+                        array[min_index] = tmp;
+                        print_array(array, size);
+                }
+        }
 }
