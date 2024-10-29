764a1778617ffc235bf89b92d6fb9d3c3a108c24
doehab <dohaehab@gmail.com>
2024-10-29 14:04:55 -0700

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -64,5 +64,7 @@ void quick_sort_recursion(int *arr, int low, int high, size_t size)
 
 void quick_sort(int *array, size_t size)
 {
+	if (array == NULL || size < 2)
+		return;
 	quick_sort_recursion(array, 0, size - 1, size);
 }
