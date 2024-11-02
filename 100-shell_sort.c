a432bc92aed2a0929965a9a57b9cfa99900a6f98
doehab <dohaehab@gmail.com>
2024-11-02 16:16:25 -0700

    shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -10,19 +10,29 @@
 
 void shell_sort(int *array, size_t size)
 {
-	size_t gap, i, j;
+	size_t gap = 1;
+	size_t i, j;
 	int temp;
 
-	for (gap = size / 2; gap > 0; gap /= 2)
+	if (array == NULL)
+		return;
+
+	while (gap < size / 3)
+	{
+		gap = 3 * gap + 1;
+	}
+	while (gap > 0)
 	{
 		for (i = gap; i < size; i++)
 		{
 			temp = array[i];
-			
 			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			{
 				array[j] = array[j - gap];
+			}
 			array[j] = temp;
 		}
+		gap = (gap - 1) / 3;
 		print_array(array, size);
 	}
 }
